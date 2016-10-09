cd7ce86260d4fd3fd4affbcf451dea7f03f7ff94
kaizensauce <kaizensauce@gmail.com>
2016-10-09 13:48:51 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -22,7 +22,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var app = (0, _express2.default)();
 
 //mongoose.connect('mongodb://localhost/pomodoros');
-mongodb://heroku_00msbsnf:2tle7klvn5952bngv7mg5178rl@ds033966.mlab.com:33966/heroku_00msbsnf/pomodoros
+//mongodb://heroku_00msbsnf:2tle7klvn5952bngv7mg5178rl@ds033966.mlab.com:33966/heroku_00msbsnf/pomodoros
 //_mongoose2.default.connect('mongodb://mongodb:27017/pomodoros');
 _mongoose2.default.connect('mongodb://heroku_00msbsnf:2tle7klvn5952bngv7mg5178rl@ds033966.mlab.com:33966/heroku_00msbsnf/pomodoros');
 
